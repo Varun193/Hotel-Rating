b71d2f3c37af3542765b06967cf2a58d38340b33
“varun <varunraj1935@gmail.com>
2023-03-15 22:19:12 +0530

    commiting user service impl(invoking two services together Rating and Hotel)

--- UserService/UserService/src/main/java/com/user_service/service/impl/UserServiceImpl.java
+++ UserService/UserService/src/main/java/com/user_service/service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.user_service.service.impl;
 
 import com.netflix.discovery.converters.Auto;
+import com.user_service.entites.Hotel;
+import com.user_service.entites.Rating;
 import com.user_service.entites.User;
 import com.user_service.repo.UserRepo;
 import com.user_service.service.UserService;
@@ -15,6 +17,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -27,6 +30,8 @@ public class UserServiceImpl implements UserService {
 
 //    private Logger logger = (Logger) LoggerFactory.getLogger(UserServiceImpl.class);
 
+
+    Rating rating;
 
     @Override
     public User saveUser(User user) {
@@ -48,9 +53,20 @@ public class UserServiceImpl implements UserService {
         //http://localhost:8083/ratings/users/d083c5e1-8e9f-41ed-95e2-f041492c543e
         User user = userRepo.findById(userId).get();
         //invoking rating service to get rating of particular user by Userid
-        ArrayList ratings = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getId(), ArrayList.class);
-        System.out.println(ratings.toString());
-        user.setRating(ratings);
+        Rating[] ratingofUser = restTemplate.getForObject("http://localhost:8083/ratings/users/" + user.getId(), Rating[].class);
+//        System.out.println(ratings.toString());
+        List<Rating> ratings = Arrays.stream(ratingofUser).collect(Collectors.toList());
+
+        List<Rating> ratingList = ratings.stream().map(rating -> {
+            System.out.println(rating.getHotelId());
+            //invoke Hotel Service to get rating of particular user and map
+            //http://localhost:8083/ratings/hotels/6cc93cac-3a49-408b-98d0-4ee5bf1290c7
+            ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://localhost:8082/hotels/"+ rating.getHotelId(), Hotel.class);
+            Hotel hotel = forEntity.getBody();
+            rating.setHotel(hotel);
+            return rating;
+        }).collect(Collectors.toList());
+        user.setRating(ratingList);
         return user;
     }
 
